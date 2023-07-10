6f07f000ca7edbfa6d079eb7e5a9833b5c93c778
demiresaa <demiresa38@gmail.com>
2023-07-10 14:01:57 +0300

    first commit

---
+++ src/Pokemoncard.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+const POKE_API =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/ca5a7886c10753144e6fae3b69d45a4d42a449b4/sprites/pokemon";
+
+class Pokemoncard extends Component {
+  render() {
+    let imgSrc = `${POKE_API}/${this.props.id}.png`;
+
+    return (
+      <div className=" Pokecard">
+        <h1 className="PokecardTitle">Pokemon</h1>
+        <div className="PokecardImage">
+          <img src={imgSrc} alt={this.props.name} />
+        </div>
+        <div className="PokemoncardData"> Type :{this.props.type}</div>
+        <div className="PokemoncardData">Exp :{this.props.exp}</div>
+      </div>
+    );
+  }
+}
+export default Pokemoncard;
